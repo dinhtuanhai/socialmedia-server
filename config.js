399b07903f2d7b4222f3c78efb7a3a93cd34aff8
dinhtuanhai <dinhtuanhai1298@gmail.com>
2021-01-28 09:47:54 +0700

    Add configjs file

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://classsed:hQKRlf5nKcG6WmJB@cluster0.boyve.mongodb.net/socialmedia?retryWrites=true&w=majority',
+    SECRET_KEY: 'this is a super super secret key'
+}
